7f34546d6dbc7b957d0ba5df8170f647c2e8f704
Jason Hu <jason431@ucla.edu>
2020-03-18 19:33:50 +0800

    trying to append json files together

--- combinejson.py
+++ combinejson.py
@@ -2,27 +2,41 @@ import json
 
 if __name__ == "__main__":
 
-    obj = open('allJsonAppendedTogether.json', 'w+')
+    obj = open('allJsonAppendedTogether.json', 'r+')
+    a = json.load(obj)
+    print(a)
+    b = a['rows']
+    print(b)
+    b.append("hello")
+    print(b)
 
-    with open('pg1.json', 'r') as jsonStr:
+    """with open('pg1.json', 'r') as jsonStr:
         jsonStrText = json.load(jsonStr)
-        obj.write(json.dumps(jsonStrText))
+        for idIndex in range(0, 20):
+            rowInfo = jsonStrText['rows'][idIndex]
+            print(rowInfo)
+            json.dump(rowInfo, obj)"""
 
-    #print(json.load(obj))
+    """with open('allJsonAppendedTogether.json', 'r+') as obj:
+    
+
+        data = json.load(obj)
+        print(data['rows'][1])"""
+
+
+
 
     """
-    pgNum = 2
-
-    while pgNum <= 5:
-        with open('pg' + str(pgNum) + '.json', 'r') as jsonStr:
-            jsonStrText = json.load(jsonStr)
-            for dataRow in jsonStrText['rows']:
-                dataRow['id'] = pgNum * 10 + int(dataRow['id'])
-                # TODO: append data rows to the allJsonAppendedTogether.json file
-        pgNum += 1"""
-
+        pgNum = 2
+    
+        while pgNum <= 5:
+            with open('pg' + str(pgNum) + '.json', 'r') as jsonStr:
+                jsonStrText = json.load(jsonStr)
+                for dataRow in jsonStrText['rows']:
+                    dataRow['id'] = pgNum * 10 + int(dataRow['id'])
+                    # TODO: append data rows to the allJsonAppendedTogether.json file
+            pgNum += 1"""
     obj.close()
-
     #TODO:
     # 1. Make a new file that is for combining all the json stuff
     # 2. Open pg1 and extract data from pg1 to new file
