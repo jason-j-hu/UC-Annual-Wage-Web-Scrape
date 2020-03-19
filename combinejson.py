258e701afae861e0ca490d19aa848c9bfe82ad00
Jason Hu <jason431@ucla.edu>
2020-03-19 20:44:34 +0800

    need to update combinejson file code

--- combinejson.py
+++ combinejson.py
@@ -2,20 +2,36 @@ import json
 
 if __name__ == "__main__":
 
-    obj = open('allJsonAppendedTogether.json', 'r+')
-    a = json.load(obj)
-    print(a)
-    b = a['rows']
-    print(b)
-    b.append("hello")
-    print(b)
 
-    """with open('pg1.json', 'r') as jsonStr:
+
+
+    """with open('allJsonAppendedTogether.json', 'r') as fp:
+        information = json.load(fp)
+
+    information['rows'].append({
+        #data here
+    })
+
+    with open('allJsonAppendedTogether.json', 'w') as fp:
+        json.dumps(information)"""
+
+
+
+
+
+    """obj = open('allJsonAppendedTogether.json', 'r+')
+    mergedFile = json.load(obj)
+    b = print(mergedFile)
+    mergedFile['rows'].append("hello")
+    print(mergedFile['rows'])
+    json.dump(b, obj)"""
+
+    with open('pg1.json', 'r') as jsonStr:
         jsonStrText = json.load(jsonStr)
         for idIndex in range(0, 20):
             rowInfo = jsonStrText['rows'][idIndex]
             print(rowInfo)
-            json.dump(rowInfo, obj)"""
+            #json.dump(rowInfo, obj)
 
     """with open('allJsonAppendedTogether.json', 'r+') as obj:
     
@@ -36,7 +52,7 @@ if __name__ == "__main__":
                     dataRow['id'] = pgNum * 10 + int(dataRow['id'])
                     # TODO: append data rows to the allJsonAppendedTogether.json file
             pgNum += 1"""
-    obj.close()
+    #obj.close()
     #TODO:
     # 1. Make a new file that is for combining all the json stuff
     # 2. Open pg1 and extract data from pg1 to new file
